70fb95cf1c3393273b1acfbf4a1cbc7534b984fc
chandhu <chandhudev0@gmail.com>
2023-05-29 16:43:15 +0530

    fixes the userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,18 +6,20 @@ const passport=require("passport")
 const {failure,getUserInfo,logout,getUsers}=require("../controllers/userController")
 
 router.get("/google",passport.authenticate("google",["profile","email"]))
-router.get("/google/callback",passport.authenticate("google",{failureRedirect:"/login/failure"}),(req,res) => {
+router.get("/google/callback",passport.authenticate("google",{failureRedirect:"/login/failure"}),async(req,res) => {
     const userData = {
         username: req.user._json.name,
         email: req.user._json.email,
         profileImage: req.user._json.picture,
       };
-
-   User.findOne({ email: userData.email }, (err, user) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Error checking if user exists')
-        } else if (!user) {
+      const user = await User.findOne({ email: userData.email }).exec()
+      try{
+        if(user){
+          console.log('User',user._id);
+          res.cookie('userId', user._id);
+          res.redirect(process.env.CLIENT_URL);
+        }
+        else{
           // Save user data to MongoDB
           const newUser = new User(userData);
           newUser.save()
@@ -27,12 +29,12 @@ router.get("/google/callback",passport.authenticate("google",{failureRedirect:"/
             res.redirect(process.env.CLIENT_URL);
           })
           .catch(err => console.error(err));
-        } else {
-          console.log('User',user._id);
-          res.cookie('userId', user._id);
-          res.redirect(process.env.CLIENT_URL);
         }
-      })
+      }
+      catch(err){
+        console.error(err.message);
+        res.status(500).send('Error checking if user exists')
+      }
 })
 
 router.route("/login/failure").get(failure)
